0d842ea4d36b4d6fedd484d7b736e2d4f5e9410c
Giovanni Allegri <giohappy@gmail.com>
2018-02-26 12:44:03 +0100

    rename basemaps

--- nfdclient/js/ms2Override/ClevMetroLayer.js
+++ nfdclient/js/ms2Override/ClevMetroLayer.js
@@ -11,8 +11,8 @@ var L = require('leaflet');
 
 Layers.registerType('clevmetro', (opt) => {
     const clevUrls = {
-        Map: 'https://api.mapbox.com/styles/v1/cleveland-metroparks/cisvvmgwe00112xlk4jnmrehn/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2xldmVsYW5kLW1ldHJvcGFya3MiLCJhIjoiWHRKaDhuRSJ9.FGqNSOHwiCr2dmTH2JTMAA',
-        Arial: 'https://api.mapbox.com/styles/v1/cleveland-metroparks/cjcutetjg07892ro6wunp2da9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2xldmVsYW5kLW1ldHJvcGFya3MiLCJhIjoiWHRKaDhuRSJ9.FGqNSOHwiCr2dmTH2JTMAA'
+        metroparksMap: 'https://api.mapbox.com/styles/v1/cleveland-metroparks/cisvvmgwe00112xlk4jnmrehn/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2xldmVsYW5kLW1ldHJvcGFya3MiLCJhIjoiWHRKaDhuRSJ9.FGqNSOHwiCr2dmTH2JTMAA',
+        metroparksAerial: 'https://api.mapbox.com/styles/v1/cleveland-metroparks/cjcutetjg07892ro6wunp2da9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2xldmVsYW5kLW1ldHJvcGFya3MiLCJhIjoiWHRKaDhuRSJ9.FGqNSOHwiCr2dmTH2JTMAA'
     };
     return L.tileLayer(clevUrls[opt.name], opt);
 });
