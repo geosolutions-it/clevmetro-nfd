105d1dc0dbc9e32dbfbd4ebf9496568d3fedeb45
giohappy <giohappy@gmail.com>
2018-09-19 11:37:06 +0200

    changed restart.sh according to initctl setup

--- nfdapi/restart.sh
+++ nfdapi/restart.sh
@@ -1,9 +1,6 @@
 ./manage.py makemigrations
 ./manage.py migrate
-# ./manage.py compilemessages
+./manage.py compilemessages
 ./manage.py collectstatic --noinput
 
-sudo systemctl restart slapd
-sudo systemctl restart apache2
-sudo systemctl restart metroparksnfd.service
-sudo systemctl restart uwsgi
+sudo initctl restart metroparksnfd-uwsgi
